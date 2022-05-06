72c1eb40677e3db716e17d6db9cf6efe37491502
Dmitrii Nemtsov <dimas224@yandex.ru>
2022-05-06 09:34:39 +0300

    Перенес транзакции в сервис.

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -1,22 +1,17 @@
 package web.dao;
 
 import java.util.List;
-import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
 public interface UserDao {
-    @Transactional
+
     List<User> findAll();
 
-    @Transactional
     User findById(long id);
 
-    @Transactional
     void save(User user);
 
-    @Transactional
     void update(long id, User updatedUser);
 
-    @Transactional
     void delete(long id);
 }
--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,17 +1,23 @@
 package web.service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
 public interface UserService {
 
+    @Transactional(readOnly = true)
     List<User> findAll();
 
+    @Transactional(readOnly = true)
     User findById(long id);
 
+    @Transactional
     void save(User user);
 
+    @Transactional
     void update(long id, User updatedUser);
 
+    @Transactional
     void delete(long id);
 }
